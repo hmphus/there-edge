4c6ed46a461c7f27436495bfdb6e5f70a9fa2ea1
Brian Gontowski <brian@gontowski.com>
2021-07-08 22:27:52 +0900

    Added last tricks and turn indicators

--- Resources/sg/hud_spades.js
+++ Resources/sg/hud_spades.js
@@ -4,6 +4,7 @@ class CardSet {
     self.id = id;
     self.name = name;
     self.settings = Object.assign({}, {
+      sorted: true,
       selectable: false,
     }, settings ?? {});
     self.element = $(`.cardset[data-id="${self.id}"]`);
@@ -24,37 +25,60 @@ class CardSet {
           self.cards = Array(Number(self.text.substr(1))).fill('??');
         } else {
           self.cards = self.text.match(/.{1,2}/g);
-          self.cards.sort(function(a, b) {
-            const suitA = a[1];
-            const suitB = b[1];
-            if (suitA != suitB) {
-              return self.suits.indexOf(suitA) - self.suits.indexOf(suitB);
-            }
-            const rankA = a[0];
-            const rankB = b[0];
-            return self.ranks.indexOf(rankA) - self.ranks.indexOf(rankB);
-          });
+          if (self.settings.sorted) {
+            self.cards.sort(function(a, b) {
+              const suitA = a[1];
+              const suitB = b[1];
+              if (suitA != suitB) {
+                return self.suits.indexOf(suitA) - self.suits.indexOf(suitB);
+              }
+              const rankA = a[0];
+              const rankB = b[0];
+              return self.ranks.indexOf(rankA) - self.ranks.indexOf(rankB);
+            });
+          }
         }
-        //<div class="cardset" data-id="hand1" data-deck="std" data-highlighted="1">
-        //<div class="card" data-id="3h" data-selected="1"><span></span><span></span></div>
         $(self.element).empty();
         for (let card of self.cards) {
           let cardDiv = $('<div class="card"><span></span><span></span></div>');
           $(cardDiv).attr('data-id', card);
           $(self.element).prepend($(cardDiv));
           if (self.settings.selectable) {
-            $(cardDiv).on('click', function() {
-              let selected = $(this).attr('data-selected');
-              $(self.element).find('.card').attr('data-selected', '0');
+            $(cardDiv).on('mouseover', function() {
+              There.playSound('enabled menu item rollover');
+            }).on('mousedown', function(event) {
+              There.playSound('menu item activate');
+            }).on('click', function() {
+              There.clearNamedTimer('card-click');
+              if (There.data.game.state == 'playsend') {
+                return;
+              }
+              let selected = $(cardDiv).attr('data-selected');
               if (selected != 1) {
-                $(this).attr('data-selected', '1');
+                $(self.element).find('.card').attr('data-selected', '0');
+                $(cardDiv).attr('data-selected', '1');
+              } else {
+                There.setNamedTimer('card-click', 100, function() {
+                  $(cardDiv).attr('data-selected', '0');
+                });
               }
             }).on('dblclick', function() {
+              There.clearNamedTimer('card-click');
+              if (There.data.game.state == 'playsend') {
+                return;
+              }
+              $(cardDiv).attr('data-selected', '1');
+              There.data.game.playCard($(cardDiv).attr('data-id'));
             });
           }
         }
       }
     }
+  }
+
+  get selected() {
+    let self = this;
+    return jQuery.map($(self.element).find('.card[data-selected="1"]'), e => $(e).attr('data-id'));
   }
 }
 
@@ -71,7 +95,9 @@ class Game {
         new CardSet('played3', 'played3'),
         new CardSet('played4', 'played4'),
       ],
-      lasttrick: new CardSet('lasttrick', 'lasttrick'),
+      lasttrick: new CardSet('lasttrick', 'lasttrick', {
+        sorted: false,
+      }),
     };
   }
 
@@ -107,6 +133,7 @@ class Game {
         let state = gameData.state.toLowerCase();
         if (self.state != state) {
           self.state = state;
+          self.resetIndicators();
           if (self.state == 'endgame' && self.previousGameStatesCount > 0) {
             There.playSound('cards game over');
           }
@@ -116,10 +143,10 @@ class Game {
         self.isDealer = (Number(gameData.dealer) - 1 == self.thisPlayer && self.thisPlayer >= 0);
         self.isHost = (Number(gameData.host) - 1 == self.thisPlayer && self.thisPlayer >= 0);
         $('.hud').attr('data-gamestate', self.state).attr('data-isactiveplayer', self.isActivePlayer ? '1' : '0');
-        $('.button[data-id="newgame"]').attr('data-enabled', self.isHost ? '1' : '0');
-        $('.button[data-id="deal"]').attr('data-enabled', self.isActivePlayer && self.state == 'deal' ? '1' : '0');
-        $('.button[data-id="play"]').attr('data-enabled', self.isActivePlayer && self.state == 'play' ? '1' : '0');
-        $('.button[data-id="taketrick"]').attr('data-enabled', self.isActivePlayer && self.state == 'taketrick' ? '1' : '0');
+        $('.left .panel[data-id="game"] .button[data-id="newgame"]').attr('data-enabled', self.isHost ? '1' : '0');
+        $('.left .panel[data-id="game"] .button[data-id="deal"]').attr('data-enabled', self.isActivePlayer && self.state == 'deal' ? '1' : '0');
+        $('.left .panel[data-id="game"] .button[data-id="play"]').attr('data-enabled', self.isActivePlayer && self.state == 'play' ? '1' : '0');
+        $('.left .panel[data-id="game"] .button[data-id="taketrick"]').attr('data-enabled', self.isActivePlayer && self.state == 'taketrick' ? '1' : '0');
         for (let player of self.players) {
           {
             let playerDiv = $(`.left .panel[data-id="tricks"] .players .player[data-player="${player.id}"]`);
@@ -141,32 +168,100 @@ class Game {
             There.data.cardsets.hand.onData('cardset', cardsetData);
           }
         }
+        self.showIndicators();
       }
+    }
+  }
+
+  resetIndicators() {
+    let self = this;
+    self.blinkCount = 0;
+    self.clearIndicators();
+  }
+
+  clearIndicators() {
+    let self = this;
+    $('.left .panel[data-id="game"] .button').attr('data-highlighted', '0');
+    $('.middle .table .turn').attr('data-visible', '0');
+    $('.cardset[data-id^="player"]').attr('data-highlighted', '0');
+  }
+
+  showIndicators() {
+    let self = this;
+    let duration = 0;
+    let isBlink = false;
+    if (self.isActivePlayer && self.state != 'playsend') {
+      if (self.blinkCount % 2 == 0) {
+        duration = 2000;
+        isBlink = false;
+      } else {
+        duration = 150;
+        isBlink = true;
+        if (self.blinkCount < 6 || self.blinkCount % 10 == 5) {
+          There.playSound('cards your turn')
+        }
+      }
+      self.blinkCount++;
+    }
+    self.clearIndicators();
+    if (self.activePlayer >= 0) {
+      const activePlayer = self.players[self.activePlayer];
+      switch (self.state) {
+        case 'deal': {
+          if (isBlink) {
+            $('.left .panel[data-id="game"] .button[data-id="deal"]').attr('data-highlighted', '1');
+          }
+          break;
+        }
+        case 'bid': {
+          if (isBlink) {
+            $('.left .panel[data-id="game"] .button[data-id="bid"]').attr('data-highlighted', '1');
+          }
+          break;
+        }
+        case 'play': {
+          if (isBlink) {
+            $('.left .panel[data-id="game"] .button[data-id="play"]').attr('data-highlighted', '1');
+          } else {
+            $(`.middle .table[data-player="${activePlayer.id}"] .turn`).attr('data-visible', '1');
+          }
+          break;
+        }
+        case 'taketrick': {
+          if (isBlink) {
+            $('.left .panel[data-id="game"] .button[data-id="play"]').attr('data-highlighted', '1');
+          }
+          $(`.cardset[data-id="player${activePlayer.id}"]`).attr('data-highlighted', '1');
+          break;
+        }
+      }
+    }
+    if (duration == 0) {
+      There.clearNamedTimer('turn-indicator');
+    } else {
+      There.setNamedTimer('turn-indicator', duration, function() {
+        self.showIndicators();
+      });
+    }
+  }
+
+  playCard(card) {
+    let self = this;
+    if (self.state != 'play') {
+      return;
     }
   }
 }
 
 $(document).ready(function() {
-  $('.left .logo').on('click', function() {
-    if (There.variables.maxpoints != undefined) {
-      There.data.messages.addMessage(0, `Playing until ${There.variables.maxpoints} points.`);
-    }
-    if (There.variables.host != undefined) {
-      There.data.messages.addMessage(0, `Player ${There.variables.host} is host.`);
-    }
-    if (There.variables.round != undefined) {
-      There.data.messages.addMessage(0, `Round ${There.variables.round}.`);
-    }
-  });
-
-  $('.button[data-id="newgame"]').on('click', function() {
+  $('.left .panel[data-id="game"] .button[data-id="newgame"]').on('click', function() {
     if ($(this).attr('data-enabled') == 0) {
       return;
     }
     There.sendEventMessageToClient(1);
   });
 
-  $('.button[data-id="rules"]').on('click', function() {
+  $('.left .panel[data-id="game"] .button[data-id="rules"]').on('click', function() {
     const rulesPath = There.data.channels?.hudconfig?.data?.rulesurl;
     if (rulesPath != undefined) {
       There.guiCommand({
@@ -176,14 +271,14 @@ $(document).ready(function() {
     }
   });
 
-  $('.button[data-id="deal"]').on('click', function() {
+  $('.left .panel[data-id="game"] .button[data-id="deal"]').on('click', function() {
     if ($(this).attr('data-enabled') == 0) {
       return;
     }
     There.sendEventMessageToClient(2);
   });
 
-  $('.button[data-id="play"]').on('click', function() {
+  $('.left .panel[data-id="game"] .button[data-id="play"]').on('click', function() {
     if ($(this).attr('data-enabled') == 0) {
       return;
     }
@@ -191,20 +286,22 @@ $(document).ready(function() {
       There.data.messages.addMessage(0, `It isn't your turn to play a card.`);
       return;
     }
-    if (There.data.game.playersHand?.selectedCards?.length ?? 0 == 0) {
+    let selectedCard = There.data.cardsets.hand?.selected ?? [];
+    if (selectedCard.length == 0) {
       There.data.messages.addMessage(0, `Please select a card to be played.`);
       return;
     }
+    There.data.game.playCard(selectedCard[0]);
   });
 
-  $('.button[data-id="taketrick"]').on('click', function() {
+  $('.left .panel[data-id="game"] .button[data-id="taketrick"]').on('click', function() {
     if ($(this).attr('data-enabled') == 0) {
       return;
     }
     There.sendEventMessageToClient(5);
   });
 
-  $('.button[data-id="bid"]').on('click', function() {
+  $('.left .panel[data-id="game"] .button[data-id="bid"]').on('click', function() {
     if ($(this).attr('data-enabled') == 0) {
       return;
     }
