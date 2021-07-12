0f213e4744d17f829bb8f14284a86195a56fdc7f
Brian Gontowski <brian@gontowski.com>
2021-07-12 20:53:45 +0900

    Progress with ChangeMe Dialog

--- Resources/changeme/flashdialog.js
+++ Resources/changeme/flashdialog.js
@@ -1,9 +1,4 @@
 There.init({
-  data: {
-    queue: [],
-    isReady: false,
-  },
-
   onReady: function() {
     There.fsCommand('setStageWidthHeight', {
       width: 260,
@@ -19,46 +14,22 @@ There.init({
       depth: 32,
     });
   },
+});
 
-  onVariable: function(name, value) {
-    There.data.queue.push({name: name, value: value});
-    if (There.data.player == undefined) {
-      if (name != 'there_ready') {
-        return;
-      }
-      const ruffle = window.RufflePlayer.newest();
-      There.data.player = ruffle.createPlayer();
-      There.data.player.config = {
-        autoplay: 'on',
-        unmuteOverlay: 'hidden',
-        contextMenu: false,
-        backgroundColor: '#0000',
-      };
-      There.data.player.onFSCommand = function(command, query) {
-        There.fsCommand(command, query);
-        if (There.data.isReady == false) {
-          There.data.isReady = true;
-          setTimeout(There.forwardRuffleVariables, 0);
-        }
-      };
-      $('body').append(There.data.player);
-      There.data.player.load({
-        url: `http://${There.variables.there_resourceshost}/resources/changeme/flashdialog.swf`,
-        allowScriptAccess: true,
-      }).then(function() {
-        $('ruffle-player').attr('data-ready', '1');
-      });
-      return;
-    }
-    There.forwardRuffleVariables();
-  },
+$(document).ready(function() {
+  const ids = [
+    'cameracontrolhelp', 'thankyou', 'leavetreatments',
+    'savelooksetchangeme', 'renamelooksetchangeme', 'deletelooksetchangeme',
+    'saveoutfitchangeme', 'renameoutfitchangeme', 'deleteoutfitchangeme',
+  ];
 
-  forwardRuffleVariables: function() {
-    if (There.data.isReady) {
-      while (There.data.queue.length > 0) {
-        const entry = There.data.queue.shift();
-        There.data.player.instance.set_variable(`_root.${entry.name}`, entry.value);
-      }
-    }
-  },
+  $('.dialog').attr('data-id', ids[0]);
+
+  $('.button[data-id="close"]').on('click', function() {
+    $('.dialog').attr('data-id', ids[((ids.indexOf($('.dialog').attr('data-id'))) + 1) % ids.length]);
+  });
+
+  $('.modes .tab').on('click', function() {
+   $(this).parents('.modes').attr('data-mode', $(this).attr('data-id'));
+  });
 });
