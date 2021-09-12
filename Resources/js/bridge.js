018affb5ba00e49d6cf4afbdb3771afb33b70452
Brian Gontowski <brian@gontowski.com>
2021-09-13 08:57:18 +0900

    Added a common bridge file for Ruffle controls

---
+++ Resources/js/bridge.js
@@ -0,0 +1,59 @@
+There.init({
+  data: {
+    queue: [],
+    isReady: false,
+  },
+
+  onVariable: function(name, value) {
+    There.data.queue.push({name: name, value: value});
+    if (There.data.player == undefined) {
+      if (name != 'there_ready') {
+        return;
+      }
+      const ruffle = window.RufflePlayer.newest();
+      There.data.player = ruffle.createPlayer();
+      There.data.player.config = {
+        autoplay: 'on',
+        unmuteOverlay: 'hidden',
+        contextMenu: false,
+        backgroundColor: '#0000',
+      };
+      There.data.player.onFSCommand = function(command, query) {
+        There.fsCommand(command, query);
+        if (There.data.isReady == false) {
+          There.data.isReady = true;
+          setTimeout(There.forwardRuffleVariables, 0);
+        }
+      };
+      $('body').append(There.data.player);
+      There.fsCommand('setStageWidthHeight', {
+        width: $(There.data.player).innerWidth(),
+        height: $(There.data.player).innerHeight(),
+      });
+      There.fsCommand('setWidthHeight', {
+        width: $(There.data.player).innerWidth(),
+        height: $(There.data.player).innerHeight(),
+      });
+      There.fsCommand('setTextureBitDepth', {
+        depth: 32,
+      });
+      There.data.player.load({
+        url: `http://${There.variables.there_resourceshost}/resources${$('body').attr('data-swf')}`,
+        allowScriptAccess: true,
+      }).then(function() {
+        $('ruffle-player').attr('data-ready', '1');
+      });
+      return;
+    }
+    There.forwardRuffleVariables();
+  },
+
+  forwardRuffleVariables: function() {
+    if (There.data.isReady) {
+      while (There.data.queue.length > 0) {
+        const entry = There.data.queue.shift();
+        There.data.player.instance.set_variable(`_root.${entry.name}`, entry.value);
+      }
+    }
+  },
+});
